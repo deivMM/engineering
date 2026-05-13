4a1f05bb3c8d2455fd4cfb73ca1cd0b5013cd1cf
deivMM <d.marquez.monj@gmail.com>
2026-05-13 15:01:04 +0200

    new_version

---
+++ estudio_cinematica.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+art_angle = 20
+PCD_tripode = 53.2
+
+rot_angles = np.linspace(0,360,361)
+
+
+
+
+z2 = PCD_tripode / 4 * np.sin(np.radians(art_angle))*np.cos(np.radians(rot_angles))
+HI = (1/4) * PCD_tripode * (1 - np.cos(np.radians(art_angle))) * np.cos(2 * np.radians(rot_angles))
+
+
+
+f, ax = plt.subplots()
+
+ax.plot(rot_angles, z2)
+ax.plot(rot_angles, HI)
+
+
+plt.show()
+
+
+
+
+
+
+
+
+
