1b7a15a1843c77d54959e5286a5d6b8169e1af08
deivMM <d.marquez.monj@gmail.com>
2026-05-14 11:39:32 +0200

    '2020214_cmmt_0'

--- 00_Readme.txt
+++ 00_Readme.txt
@@ -1,3 +1,5 @@
 git add .
 git commit -m "Descripción de los cambios"
 git push
+
+git pull
--- Estudio_cinematica/estudio_cinematica.py
+++ Estudio_cinematica/estudio_cinematica.py
@@ -1,6 +1,10 @@
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+%matplotlib qt
+# %matplotlib inline
 
 
 art_angle = 20
@@ -13,17 +17,45 @@ z2 = PCD_tripode / 4 * np.sin(np.radians(art_angle))*np.cos(np.radians(rot_angle
 HI = (1/4) * PCD_tripode * (1 - np.cos(np.radians(art_angle))) * np.cos(2 * np.radians(rot_angles))
 
 
-f, ax = plt.subplots()
 
-ax.plot(rot_angles, z2)
-ax.plot(rot_angles, HI)
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 8), facecolor='.85')
+
+
+
+ax1.plot(z2, HI)
+dot, = ax1.plot([], [], 'ro')  # red dot
+ax1.axis('equal')
+
+
+ax2.plot(rot_angles, z2)
+ax2.plot(rot_angles, HI)
+
+
+vline = ax2.axvline(color='k', lw=2, alpha=.3)
+
+def init():
+    dot.set_data([], [])
+    vline.set_xdata([])
+    return dot, vline,
+
+
+def update(frame):
+    x_val = z2[frame]
+    y_val = HI[frame]
+
+    dot.set_data([x_val], [y_val])
+    vline.set_xdata([rot_angles[frame], rot_angles[frame]])
+
+    return dot, vline,
+
+
+ani = FuncAnimation(
+    f, update, frames=len(rot_angles),
+    init_func=init, blit=True, interval=50, repeat=False
+)
 
 
 plt.show()
-
-
-
-
 
 
 
