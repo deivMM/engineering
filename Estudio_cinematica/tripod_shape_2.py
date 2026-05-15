84be1f6b5e2ccb6a4edc3d7366b49a47ef57d85e
deivMM <d.marquez.monje@gmail.com>
2026-05-15 14:24:53 +0200

    20260515_cmmt_0

--- Estudio_cinematica/tripod_shape_2.py
+++ Estudio_cinematica/tripod_shape_2.py
@@ -1,26 +1,106 @@
-from shapely.geometry import LineString
+from shapely.geometry import box, Point
+from shapely.ops import unary_union
 import matplotlib.pyplot as plt
 
-# Center line
-line = LineString([(0, 0), (42, 0)]) #It is only a 1D object.
-# start point → (0,0)
-# end point → (20,0)
+# Parámetros
+ancho_centro = 35
+alto_centro = 55
 
-# Buffer creates smooth geometry
-shape = line.buffer(
-    21,
-    cap_style=1  # round ends
+ancho_extremo = 42
+alto_extremo = 30
+
+radio = alto_extremo / 2
+
+# -------------------------
+# Parte central
+# -------------------------
+
+cuerpo = box(
+    -ancho_centro / 2,
+    -alto_centro / 2,
+    ancho_centro / 2,
+    alto_centro / 2
 )
 
-# Extract coordinates
-x, y = shape.exterior.xy
+# -------------------------
+# TOP
+# -------------------------
 
-# Plot
-fig, ax = plt.subplots()
+# Centro vertical del top
+y_top = alto_centro / 2
 
-ax.fill(x, y, alpha=0.5)
-ax.plot(x, y, color='black')
+# Rectángulo central del top
+top_rect = box(
+    -ancho_extremo / 2,
+    y_top - alto_extremo / 2,
+    ancho_extremo / 2,
+    y_top + alto_extremo / 2
+)
 
-ax.set_aspect('equal')
+# Círculo izquierdo
+left_circle = Point(
+    -ancho_extremo / 2,
+    y_top
+).buffer(radio)
+
+# Círculo derecho
+right_circle = Point(
+    ancho_extremo / 2,
+    y_top
+).buffer(radio)
+
+# Unión top
+top = unary_union([
+    top_rect,
+    left_circle,
+    right_circle
+])
+
+# -------------------------
+# BOTTOM
+# -------------------------
+
+y_bottom = -alto_centro / 2
+
+bottom_rect = box(
+    -ancho_extremo / 2,
+    y_bottom - alto_extremo / 2,
+    ancho_extremo / 2,
+    y_bottom + alto_extremo / 2
+)
+
+left_circle_b = Point(
+    -ancho_extremo / 2,
+    y_bottom
+).buffer(radio)
+
+right_circle_b = Point(
+    ancho_extremo / 2,
+    y_bottom
+).buffer(radio)
+
+bottom = unary_union([
+    bottom_rect,
+    left_circle_b,
+    right_circle_b
+])
+
+# -------------------------
+# Unión final
+# -------------------------
+
+pieza = unary_union([cuerpo, top, bottom])
+
+# Redondeo suave entre piezas
+pieza = pieza.buffer(2).buffer(-10)
+
+# -------------------------
+# Dibujar
+# -------------------------
+
+x, y = pieza.exterior.xy
+
+plt.figure(figsize=(5, 8))
+plt.fill(x, y)
+plt.axis("equal")
 plt.show()
-
